7e2c4450fa799f072d6d3c9f3309df0e80710f48
John Jomar Dimaunahan <johndjomar@gmail.com>
2023-12-07 14:49:15 +0800

    Added the caesar cypher decryption

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     private static HashMap<Character, Character> caesarMap = new HashMap<>();
     public static void main(String[] args) throws Exception {
        putValuesToMap();
        String str = "ABC";
+       System.out.println("Value to encrypt: "+str);
        String encryptedStr = encrypt(str);
-       System.out.println(encryptedStr);
+       System.out.println("Encrypted String: "+encryptedStr);
        String decryptedStr = decrypt(encryptedStr);
-       System.out.println(decryptedStr);
+       System.out.println("Decrypted String: "+decryptedStr);
 
 
         
@@ -33,20 +35,33 @@ public class Main {
     public static String decrypt(String origStr){
         String binaryCharacter = "";
         String decryptedStr= "";
+        String strContainer= "";
         int binaryIntValue= 0 ;
         int ctr = 1;
-
+        //Part for converting binary to a character
         for(int i = 0 ; i<origStr.length();i++){
             binaryCharacter+=origStr.charAt(i);
             if(ctr==8){
                 binaryIntValue = Integer.parseInt(binaryCharacter,2);
-                decryptedStr+=(char) binaryIntValue;
+                strContainer+=(char) binaryIntValue;
                 binaryCharacter = "";
                 ctr=1;
                 continue;
             }
             ctr++;
         }
+
+         for(int i = 0 ; i<strContainer.length();i++){
+            char currentChar = strContainer.charAt(i);
+            for(Map.Entry<Character, Character> entry : caesarMap.entrySet()){
+                // System.out.println("Entry key:"+entry.getKey());
+                // System.out.println("Entry value: "+entry.getValue());
+                // System.out.println("Current Char: "+currentChar);
+                if(entry.getValue() == currentChar){
+                    decryptedStr+=entry.getKey();
+                }
+            }
+         }
         return decryptedStr;
     }
     //Caesar Cyperh -3 shift
